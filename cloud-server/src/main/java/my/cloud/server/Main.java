f993b909febe49b4a2a46fb203b24d4a5cc0ec59
SttDmitry <sheicko.dmitry@yandex.ru>
2021-04-18 20:06:31 +0300

    hw3

--- cloud-server/src/main/java/my/cloud/server/Main.java
+++ cloud-server/src/main/java/my/cloud/server/Main.java
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Factory.getServerService().startServer();
         //Мои вопросы:
-
+        //1. Как работать сразу с двумя типами данных, ведь я должен отправлять и измененный список объектов на сервере (если произошел upload) и должен отправлять файл, если происходит download этого файла со стороны клиента
     }
 }
